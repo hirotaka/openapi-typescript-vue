aae68d52244f43182a8bdf48691106eea809f5c7
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2025-10-13 18:44:06 -0400

    refactor: update `api.ts` in example package
    ensure that we specify the `paths` type parameter when calling `createClient`

--- packages/openapi-vue-query/examples/vue3/src/api.ts
+++ packages/openapi-vue-query/examples/vue3/src/api.ts
@@ -3,4 +3,4 @@ import createClient from "openapi-vue-query";
 import type { paths } from "./lib/api/v1";
 
 const fetchClient = createFetchClient<paths>({ baseUrl: "/" });
-export const $api = createClient(fetchClient);
+export const $api = createClient<paths>(fetchClient);
